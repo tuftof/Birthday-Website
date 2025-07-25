552bbd8adad29a6533dbfafc565b4748324c90eb
Kristoff Pura <kristoffpura0807@gmail.com>
2025-07-26 03:11:35 +0800

    initial commit

---
+++ code.js
@@ -0,0 +1,10 @@
+function letter(){
+const box2 = document.getElementById("div2");
+const box1 = document.getElementById("div1");
+
+box2.style.visibility = "visible";
+box2.style.animationName = "fadeIn";
+box2.style.animationDuration = "2s";
+box1.style.visibility = "hidden";
+}
+
