434962f06cdf6d2f2c93710a51bddf0c5ef8f2a0
Kristoff Pura <kristoffpura0807@gmail.com>
2025-07-26 19:41:57 +0800

    Refactor letter display logic and enhance animations; add new image for letter

--- code.js
+++ code.js
@@ -10,12 +10,41 @@ box1.style.visibility = "hidden";
 
 function readLetter(){
 const box2 = document.getElementById("letterdiv");
-const box3 = document.getElementById("div2");       
+const box3 = document.getElementById("wrapper2"); 
 
+
+box3.style.visibility = "visible";
 box2.style.visibility = "hidden";
 box3.style.animationName = "fadeIn";
 box3.style.animationDuration = "2s";
-box3.style.visibility = "visible";
+
+}
+
+function openLetter() {
+    const first = document.getElementById("firsthalf");``
+    const second = document.getElementById("secondhalf");
+    const foryou = document.getElementById("forYou");
+
+    first.style.transform = "translate(-50%,-100%) rotateX(0deg)";
+    second.style.transform = "translate(-50%,-5%) ";
+
+
+
+    setTimeout(() => {
+        foryou.style.visibility = "hidden";
+        
+    }, 1000); // Adjust the delay as needed
+        
+    setTimeout(() => {
+        const bodyLetter = document.getElementById("bodyLetter");
+        bodyLetter.style.visibility = "visible";
+        bodyLetter.style.animationName = "fadeIn";
+        bodyLetter.style.animationDuration = "2s";
+
+        bodyLetter.style.transform = "translate(-50%, -50%)";
+
+    }, 1500);
+      
 }
 
 document.addEventListener('mousemove',function(e){
