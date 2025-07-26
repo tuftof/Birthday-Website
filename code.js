9ca6cf57cde08eab4d3f89937d39f5f209590095
Kristoff Pura <kristoffpura0807@gmail.com>
2025-07-26 15:58:38 +0800

    second commit

--- code.js
+++ code.js
@@ -1,5 +1,5 @@
 function letter(){
-const box2 = document.getElementById("div2");
+const box2 = document.getElementById("letterdiv");
 const box1 = document.getElementById("div1");
 
 box2.style.visibility = "visible";
@@ -8,3 +8,34 @@ box2.style.animationDuration = "2s";
 box1.style.visibility = "hidden";
 }
 
+function readLetter(){
+const box2 = document.getElementById("letterdiv");
+const box3 = document.getElementById("div2");       
+
+box2.style.visibility = "hidden";
+box3.style.animationName = "fadeIn";
+box3.style.animationDuration = "2s";
+box3.style.visibility = "visible";
+}
+
+document.addEventListener('mousemove',function(e){
+            let body = document.querySelector('body');
+            let Heart = document.createElement('span');
+            let x = e.offsetX;
+            let y = e.offsetY;
+            Heart.style.left = x+'px';
+            Heart.style.top = y+'px';
+
+            let size = Math.random() * 10;
+            Heart.style.width = 20 + size+'px';
+            Heart.style.height = 20 + size+'px';
+
+            let transformValue = Math.random() * 360;
+            Heart.style.transform = 'rotate('+ transformValue +' deg)';
+
+            body.append(Heart);
+
+            setTimeout(function() {
+                Heart.remove();
+            }, 1000);
+        })
