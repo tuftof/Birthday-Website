fdc6f53873d53f500abfd8fcd5a9d2d46109d9be
Kristoff Pura <kristoffpura0807@gmail.com>
2025-07-28 00:24:24 +0800

    added dashboard

--- code.js
+++ code.js
@@ -11,7 +11,8 @@ box1.style.visibility = "hidden";
 function readLetter(){
 const box2 = document.getElementById("letterdiv");
 const box3 = document.getElementById("wrapper2"); 
-
+const shadow = document.getElementById("shadow");
+    shadow.style.visibility = "visible";
 
 box3.style.visibility = "visible";
 box2.style.visibility = "hidden";
@@ -24,17 +25,23 @@ function openLetter() {
     const first = document.getElementById("firsthalf");``
     const second = document.getElementById("secondhalf");
     const foryou = document.getElementById("forYou");
+    const shadow = document.getElementById("shadow");
 
     first.style.transform = "translate(-50%,-100%) rotateX(0deg)";
-    second.style.transform = "translate(-50%,-5%) ";
-
+    second.style.transform = "translate(-50%,-3%) ";
+    shadow.style.visibility = "hidden";
+    shadow.style.height = "40rem";
+    shadow.style.transform = "translate(-50%, -51%)";   
 
 
     setTimeout(() => {
         foryou.style.visibility = "hidden";
         
     }, 1000); // Adjust the delay as needed
-        
+    setTimeout(() => {
+            shadow.style.visibility = "visible";
+
+    }, 2000); // Adjust the delay as needed   
     setTimeout(() => {
         const bodyLetter = document.getElementById("bodyLetter");
         bodyLetter.style.visibility = "visible";
@@ -44,8 +51,33 @@ function openLetter() {
         bodyLetter.style.transform = "translate(-50%, -50%)";
 
     }, 1500);
-      
+    setTimeout(() => {
+        const next = document.getElementById("Next");
+        next.style.visibility = "visible";
+        next.style.animationName = "fadeIn";
+        next.style.animationDuration = "2s";
+
+    }, 7000); // Adjust the delay as needed
 }
+
+function homepage() {
+    const wrapper2 = document.getElementById("wrapper2");
+    const shadow = document.getElementById("shadow");
+    const bodyLetter = document.getElementById("bodyLetter");
+    const next = document.getElementById("Next");
+    
+    wrapper2.style.visibility = "hidden";
+    shadow.style.visibility = "hidden";
+    bodyLetter.style.visibility = "hidden";
+    next.style.visibility = "hidden";
+
+    const dashboard = document.getElementById("dashboard");
+    dashboard.style.visibility = "visible";
+    dashboard.style.animationName = "fadeIn";
+    dashboard.style.animationDuration = "2s";
+}
+
+
 
 document.addEventListener('mousemove',function(e){
             let body = document.querySelector('body');
