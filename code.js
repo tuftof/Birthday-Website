b4101a7b322e25bb5b8ee60d53056ef35a428808
Kristoff Pura <kristoffpura0807@gmail.com>
2025-08-25 22:11:26 +0800

    initial commit

--- code.js
+++ code.js
@@ -1,27 +1,64 @@
-function letter(){
-const box2 = document.getElementById("letterdiv");
-const box1 = document.getElementById("div1");
 
-box2.style.visibility = "visible";
-box2.style.animationName = "fadeIn";
-box2.style.animationDuration = "2s";
-box1.style.visibility = "hidden";
+
+function letter(){
+    const box2 = document.getElementById("letterdiv");
+    const box1 = document.getElementById("div1");
+
+    box2.style.visibility = "visible";
+    box2.style.animationName = "fadeIn";
+    box2.style.animationDuration = "2s";
+    box1.style.visibility = "hidden";
 }
 
 function readLetter(){
-const box2 = document.getElementById("letterdiv");
-const box3 = document.getElementById("wrapper2"); 
-const shadow = document.getElementById("shadow");
+
+    const backLetter = document.getElementById("backLetter");
+    
+    const LetterBack = document.getElementById("LetterBack");
+
+    LetterBack.style.visibility = "hidden";
+    backLetter.style.visibility = "hidden";
+    
+
+    const gallery = document.getElementById("gallery");
+    const board_content = document.getElementById("board_content");
+    const scrollGallery = document.getElementById("scrollGallery");
+
+    scrollGallery.style.width = "0px";
+    scrollGallery.style.visibility = "hidden";
+    board_content.style.visibility = "hidden";
+    gallery.style.visibility = "hidden";
+
+
+    const box2 = document.getElementById("letterdiv");
+    const box3 = document.getElementById("wrapper2"); 
+    const shadow = document.getElementById("shadow");
     shadow.style.visibility = "visible";
 
-box3.style.visibility = "visible";
-box2.style.visibility = "hidden";
-box3.style.animationName = "fadeIn";
-box3.style.animationDuration = "2s";
+
+
+
+    box3.style.visibility = "visible";
+    box2.style.visibility = "hidden";
+    box3.style.animationName = "fadeIn";
+    box3.style.animationDuration = "2s";
+
+
+    
+    
+
 
 }
 
+
+
+   
+
+
+
 function openLetter() {
+
+    
     const first = document.getElementById("firsthalf");``
     const second = document.getElementById("secondhalf");
     const foryou = document.getElementById("forYou");
@@ -52,29 +89,94 @@ function openLetter() {
 
     }, 1500);
     setTimeout(() => {
-        const next = document.getElementById("Next");
-        next.style.visibility = "visible";
-        next.style.animationName = "fadeIn";
-        next.style.animationDuration = "2s";
+        const nextDiv = document.getElementById("nextDiv");
+        nextDiv.style.visibility = "visible";
+        nextDiv.style.animationName = "fadeIn";
+        nextDiv.style.animationDuration = "2s";
+
+        const scroll = document.getElementById("scroll");
+        scroll.style.width = "100px";
+        scroll.style.visibility = "visible";
+        
+
 
     }, 7000); // Adjust the delay as needed
+
+    setTimeout(() => {
+            const next = document.getElementById("Next");
+            next.style.visibility = "visible";
+            next.style.animationName = "fadeIn";
+            next.style.animationDuration = "2s";
+        }, 8000);
 }
 
 function homepage() {
-    const wrapper2 = document.getElementById("wrapper2");
+
+
+    const first = document.getElementById("firsthalf");``
+    const second = document.getElementById("secondhalf");
+    const foryou = document.getElementById("forYou");
     const shadow = document.getElementById("shadow");
+    const letterScroll = document.getElementById("scroll");
+    
+    letterScroll.style.width = "0px";
+    letterScroll.style.visibility = "hidden";
+    foryou.style.visibility = "visible";
+    first.style.transform = "translate(-50%,-48%) rotateX(180deg)";
+    second.style.transform = "translate(-50%, -45%)";
+    shadow.style.height = "20rem";
+    shadow.style.transform = "translate(-50%, -45%)"; 
+
+
+    const nextDiv = document.getElementById("nextDiv");
+    const wrapper2 = document.getElementById("wrapper2");
+
     const bodyLetter = document.getElementById("bodyLetter");
     const next = document.getElementById("Next");
     
+
+    nextDiv.style.visibility = "hidden";
+    shadow.style.transform = "translate(-50%, -45%)";
     wrapper2.style.visibility = "hidden";
+    shadow.style.height = "20rem";
     shadow.style.visibility = "hidden";
     bodyLetter.style.visibility = "hidden";
     next.style.visibility = "hidden";
 
-    const dashboard = document.getElementById("dashboard");
-    dashboard.style.visibility = "visible";
-    dashboard.style.animationName = "fadeIn";
-    dashboard.style.animationDuration = "2s";
+    
+   
+    const gallery = document.getElementById("gallery");
+    const board_content = document.getElementById("board_content");
+
+    gallery.style.visibility = "visible";
+    gallery.style.animationName = "fadeIn";
+    gallery.style.animationDuration = "1s";
+
+ 
+    board_content.style.visibility = "visible";
+    board_content.style.animationName = "fadeIn";
+    board_content.style.animationDuration = "1s";
+
+    setTimeout(() => {
+        
+        const scroll = document.getElementById("scrollGallery");
+
+        scroll.style.width = "100px";
+        scroll.style.visibility = "visible";
+        
+      
+       
+        
+
+    }, 7000);
+
+      setTimeout(() => {
+            const LetterBack = document.getElementById("LetterBack");
+            LetterBack.style.visibility = "visible";
+            LetterBack.style.animationName = "fadeIn";
+            LetterBack.style.animationDuration = "2s";
+        }, 8000);
+    
 }
 
 
@@ -86,7 +188,6 @@ document.addEventListener('mousemove',function(e){
             let y = e.offsetY;
             Heart.style.left = x+'px';
             Heart.style.top = y+'px';
-
             let size = Math.random() * 10;
             Heart.style.width = 20 + size+'px';
             Heart.style.height = 20 + size+'px';
@@ -100,3 +201,6 @@ document.addEventListener('mousemove',function(e){
                 Heart.remove();
             }, 1000);
         })
+
+
+
